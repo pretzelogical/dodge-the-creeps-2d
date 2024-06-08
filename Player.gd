e59a934f98b670acfc22cedb5ee83c72295e27be
pretzelogical <yingyangyo266@gmail.com>
2024-06-08 15:13:45 -0500

    add checks for enemy collision and method for starting game

--- Player.gd
+++ Player.gd
@@ -1,4 +1,5 @@
 extends Area2D
+signal hit
 
 @export var speed = 400
 var screen_size
@@ -7,6 +8,7 @@ var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	# hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -37,3 +39,14 @@ func _process(delta):
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "up"
 		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+
+func _on_body_entered(_body):
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
